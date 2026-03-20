dd98c5509a77144585b95c296b5785864b7d9f8c
Sanjana Ramesh <sanjanaramesh66@gmail.com>
2026-03-20 14:36:51 +0530

    fixed port issue

--- app.py
+++ app.py
@@ -35,5 +35,5 @@ Text:
     return render_template("index.html", output=output, text=text)
 import os
 if __name__ == "__main__":
-    port =int(os.environ.get("PORT,10000"))
+    port =int(os.environ.get("PORT") or 10000)
     app.run(host="0.0.0.0",port=port)
